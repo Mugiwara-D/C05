d47b17d2eade15e8b7f3af119eca07f33454110e
Matiya DERUELLE <mderuell@e3r5p16.42.fr>
2021-03-26 15:21:33 +0100

    fa

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -6,24 +6,26 @@
 /*   By: mderuell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 16:30:08 by mderuell          #+#    #+#             */
-/*   Updated: 2021/03/25 16:58:18 by mderuell         ###   ########.fr       */
+/*   Updated: 2021/03/26 15:18:16 by mderuell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int ft_iterative_power(int nb, int power)
 {
 	int i;
+	int n;
 
-	i = 0;
+	n = nb;
+	i = 1;
 	while (i < power)
 	{
-		nb = nb*nb;
+		n = n*nb;
 		i++;
 	}
-	return (nb);
+	return (n);
 }
 #include <stdio.h>
 int	main()
 {
-	printf("%d\n",ft_iterative_power(2,5));
+	printf("%d\n",ft_iterative_power(5,6));
 }
