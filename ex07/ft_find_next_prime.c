2b6f4790ffdf3fe05f38096f5cf013a0d4cc38e3
Matiya DERUELLE <mderuell@e3r9p14.42.fr>
2021-03-30 16:08:41 +0200

    one shot ?

---
+++ ex07/ft_find_next_prime.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mderuell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/29 14:29:50 by mderuell          #+#    #+#             */
+/*   Updated: 2021/03/30 16:07:25 by mderuell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_is_prime(int nb)
+{
+	if (nb <= 1 && nb >= -1)
+		return (0);
+	else if ((nb % 2) == 1 || (nb % 2) == -1)
+		return (1);
+	else
+		return (0);
+}
+int ft_find_next_prime(int nb)
+{
+	int i;
+
+	i = nb;
+	while (i)
+	{
+		if (ft_is_prime(i))
+			return (i);
+		else 
+			i++;
+	}
+	return (0);
+}
+#include <stdio.h>
+int main()
+{
+	printf("%d\n", ft_find_next_prime(1561));
+}
